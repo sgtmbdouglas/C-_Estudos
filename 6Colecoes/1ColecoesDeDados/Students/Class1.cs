f4f7ae2f4050a101b4dd11625f798b344978bc4c
DCBB <dc@pop-os.localdomain>
2023-01-24 17:57:28 -0300

    colecoes de dados em c#

---
+++ 6Colecoes/1ColecoesDeDados/Students/Class1.cs
@@ -0,0 +1,24 @@
+﻿namespace Students;
+public static class Class1
+{
+    public static List<string> Start()
+    {
+        var schoolClass = new List<string>();
+        return schoolClass;
+    }
+    public static List<string> AddStudents(List<string> schooClass, string name)
+    {
+        schooClass.Add(name);
+        return schooClass;
+    }
+    public static List<string> OrderStudents(List<string> schooClass)
+    {
+        schooClass.Sort();
+        return schooClass;
+    }
+    public static List<string> RemoveStudents(List<string> schooClass, string name)
+    {
+        schooClass.Remove(name);
+        return schooClass;
+    }
+}
